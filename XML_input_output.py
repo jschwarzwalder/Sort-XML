0aa0d1354c2f107de3629d94ac52eaf8a8700f36
Jami Schwarzwalder <jamischwarzwalder@gmail.com>
2017-07-09 03:48:13 -0700

    parsing files with ElementTree

--- XML_input_output.py
+++ XML_input_output.py
@@ -2,10 +2,31 @@
 
 import os
 import sys
+import xmltodict
+import xml.etree.ElementTree as ETree
 
 directory_name = sys.argv[1]
+# run program as python XML_input_output.py "C:\\directory path"
 
 for file in os.listdir(directory_name):
     if file.endswith(".xml"):
-        print(os.path.join(directory_name, file))
+        #print(os.path.join(directory_name, file))
+        file = os.path.join(directory_name, file)
+        #with open(file) as article_to_parse:
+        #    article_dict = xmltodict.parse(article_to_parse.read())
+        
+        article = ETree.parse(file).getroot()
+            
+        #print(article_dict['root']['details']['id']['#text'] + " - " + article_dict['root']['details']['description']['#text'])
+        
+        #print(article)
+        
+        for child in article:
+            if child.tag == "details":
+                    for detail in child:
+                        if detail.tag == "description":
+                            print(detail.text)
+                        if detail.tag == "id":
+                            print(detail.text)  
+            
         
